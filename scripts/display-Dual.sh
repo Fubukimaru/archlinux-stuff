74e1dc7c2e9095a012c4ba84efeceb04c03d648e
Alberto Gutiérrez Torre <alberto.gutierrez@bsc.es>
2016-02-19 17:15:52 +0100

    Moved stuff

--- scripts/display-Dual.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if xrandr -q | grep "DP2 connected"; then
-    xrandr --output eDP1 --auto
-    xrandr --output DP2 --auto --right LVDS1
-else
-    xrandr --output eDP1 --auto
-    xrandr --output DP2 --off
-fi
