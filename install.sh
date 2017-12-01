bcc3eb8cf86ee65e9c3c61b9db08f2e377c29266
Fubukimaru <panda.fub@gmail.com>
2017-12-02 00:11:12 +0100

    added packages

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 # General packages
-sudo pacman -Syu vim tlp i3 dmenu vim doublecmd-gtk2 tpacpi-bat powetrop nmon  
+sudo pacman -Syu vim tlp i3 dmenu vim doublecmd-gtk2 tpacpi-bat powetrop nmon \
+					volumeicon arandr ctags powerline-fonts moc clementine \
+					audacious purple-facebook lxappearance texlive-most \
+					texmaker chromium
 
 # Manjaro
 sudo pacman -S yaourt 
 
 
 # Yaourt - Yaourt --noconfirm is dangerous. Use at your own risk.
-YAOURT="sudo yaourt --noconfirm -S" 
+YAOURT="yaourt --noconfirm -S" 
 $YAOURT vundle
 
 ## Science - Yaourt
-$YAOURT mendeleydesktop 
-$YAOURT rstudio-desktop 
+$YAOURT mendeleydesktop rstudio-desktop dropbox 
 
 
 
@@ -27,3 +29,7 @@ git clone https://github.com/Fubukimaru/archlinux-stuff.git
 ## Link each in place
 cd archlinux-stuff
 bash linkConfig.sh
+
+
+## Install vim plugins
+vim -c "PluginInstall"
