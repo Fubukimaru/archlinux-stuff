0e0ba4603d54ff9cd6681536d8b4e70e7d231c50
Alberto Gutiérrez Torre <alberto.gutierrez@bsc.es>
2017-12-04 11:28:16 +0100

    Added a separated script for xmodmap settings using cli

---
+++ config/xmodmap/setModif.sh
@@ -0,0 +1,10 @@
+exec --no-startup-id xmodmap -e 'keycode 66 = Mode_switch' # Block Mayus as mode switch
+exec --no-startup-id xmodmap -e 'keycode 135 = Caps_Lock NoSymbol Caps_Lock NoSymbol Caps_Lock' # LeftClick as block mayus
+exec --no-startup-id xmodmap -e 'keycode 107 = Caps_Lock NoSymbol Caps_Lock NoSymbol Caps_Lock' # ImprPant as block mayus
+
+#jklñ
+exec --no-startup-id xmodmap -e 'keycode  45 = k K Down Down lstroke Lstroke lstroke'
+exec --no-startup-id xmodmap -e 'keycode  44 = j J Left Left ssharp section ssharp'
+exec --no-startup-id xmodmap -e 'keycode  46 = l L Up Up eth ETH eth'
+exec --no-startup-id xmodmap -e 'keycode  47 = ntilde Ntilde Right Right asciitilde dead_doubleacute asciitilde dead_doubleacute ntilde Ntilde asciitilde dead_doubleacute'
+
