6df47a68c76632c0aaf124bd0ecb2c843a89eba2
Alberto Gutiérrez Torre <alberto.gutierrez@bsc.es>
2017-12-04 11:51:56 +0100

    fixed setModif and created an US version

--- config/xmodmap/setModifUS.sh
+++ config/xmodmap/setModifUS.sh
@@ -6,5 +6,6 @@ xmodmap -e 'keycode 107 = Caps_Lock NoSymbol Caps_Lock NoSymbol Caps_Lock' # Imp
 xmodmap -e 'keycode  45 = k K Down Down lstroke Lstroke lstroke'
 xmodmap -e 'keycode  44 = j J Left Left ssharp section ssharp'
 xmodmap -e 'keycode  46 = l L Up Up eth ETH eth'
-xmodmap -e 'keycode  47 = ntilde Ntilde Right Right asciitilde dead_doubleacute asciitilde dead_doubleacute ntilde Ntilde asciitilde dead_doubleacute'
+xmodmap -e 'keycode  47 = semicolon colon Right Right paragraph degree paragraph'
+
 
