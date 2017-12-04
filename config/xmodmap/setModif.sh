050ddc1799c05dc150ba0ec7f12da9da9c1cb87b
Alberto Gutiérrez Torre <alberto.gutierrez@bsc.es>
2017-12-04 19:08:44 +0100

    Ditched US Keyboard. Created < and > keys on , and . using mode switch

--- config/xmodmap/setModif.sh
+++ config/xmodmap/setModif.sh
@@ -8,3 +8,6 @@ xmodmap -e 'keycode  44 = j J Left Left ssharp section ssharp'
 xmodmap -e 'keycode  46 = l L Up Up eth ETH eth'
 xmodmap -e 'keycode  47 = ntilde Ntilde Right Right asciitilde dead_doubleacute asciitilde dead_doubleacute ntilde Ntilde asciitilde dead_doubleacute'
 
+#<> on , and . with mode switch
+xmodmap -e 'keycode 59 = comma semicolon less semicolon horizconnector multiply horizconnector'
+xmodmap -e 'keycode 60 = period colon greater colon periodcentered division periodcentered'
