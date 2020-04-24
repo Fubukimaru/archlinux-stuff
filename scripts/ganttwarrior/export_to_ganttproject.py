bc8357778b9d2e0ab4e2edced149aaf5d1795039
Fubu <agutierrez@posteo.net>
2020-04-24 12:07:40 +0200

    Script is now modularized in functions

--- scripts/ganttwarrior/export_to_ganttproject.py
+++ scripts/ganttwarrior/export_to_ganttproject.py
@@ -2,48 +2,17 @@
 import csv
 # import pytz
 import datetime
+from dateutil.parser import parse
 from tasklib import TaskWarrior
 import argparse
 from numpy import busday_count
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument(
-        '--project', '-p', metavar="[Project name]", type=str,
-        help='Set project [Mandatory]'
-)
-parser.add_argument(
-        '--set_id', '-i', type=str, default="", metavar="ID",
-        help="Set project as task with this id. TODO: This should be bool"
-)
-parser.add_argument(
-        '--output', '-o', type=str, default="tasks_gantt.csv",
-        help="CSV file for output"
-)
-
-args = parser.parse_args()
-print(args)
-print(args.project)
-if args.project is None:
-    print("Project is required")
-    parser.print_help()
-    exit()
-
-# Input config
-project = args.project
-proj_id = args.set_id   # "" -> project is not displayed
-csv_file = args.output
-
-# Default start time
-# madrid = pytz.timezone("Europe/Madrid")
-default_time_start = datetime.date(2018, 1, 1)
-default_time_end = datetime.date(2020, 7, 16)
-
-csv_columns = [
-        'ID', 'Name', 'Begin date', 'End date', 'Duration',
-        'Completion', 'Cost', 'Coordinator', 'Predecessors',
-        'Outline number', 'Resources', 'Assignments', 'Task color',
-        'Web Link', 'Notes'
-]
+# Gantt definition
+######################################
+# Project: A collection of milestones.
+# Milestone: A collection of tasks. A milestone is owned by a project.
+# Task: Definition of a task that may contain others as subtasks. Task may
+#   belong to a milestone. Task belong to a project.
 
 
 def find_task(tasks, id):
@@ -56,6 +25,15 @@ def find_task(tasks, id):
     if i == len(tasks) and tasks[i]['uuid'] == id:
         i = -1
     return(i)
+
+
+def get_tags_matching(tw, pattern="milestone_"):
+    # TODO: Find a more efficent way to do this
+    matching = set()
+    for t in tw.tasks:
+        r = set([tag for tag in t['tags'] if pattern in tag])
+        matching = matching.union(r)
+    return(matching)
 
 
 # id at first should be 0
@@ -115,63 +93,147 @@ def resolve_numbering(tasks, id_list, id, father_num):
     return(tasks)
 
 
-# Taskwarrior start
-tw = TaskWarrior()
-tasks = tw.tasks.filter("project:" + project)
+# TODO: Ask tasklib guys to implement task['end']
+def get_task_end(tw, task):
+    task_def = tw.execute_command([task['uuid']])
+    end = [txt for txt in task_def if 'End' in txt]
+    for e in end:
+        e = e.split()
+        if len(e) == 3:     # It's End + Date + Hour
+            break
+    return(parse(e[1]).date())
 
-id_list = dict()
-if proj_id != "":
-    proj_id = '0'
-    id_list[project] = proj_id
 
-tasks = build_numbering(tasks, id_list)
-prtasks = []
-if proj_id != "":
+def add_project(prtasks, proj_id, project, start_time, end_time):
     prtasks.append(
             {  # ProjectGantt will get the max and min of the dates for us
                 'ID': proj_id, 'Name': "Project: " + project,
-                'Begin date': default_time_start.strftime('%d/%m/%y'),
-                'End date': default_time_end.strftime('%d/%m/%y'),
+                'Begin date': start_time.strftime('%d/%m/%y'),
+                'End date': end_time.strftime('%d/%m/%y'),
                 'Duration': 0, "Outline number": proj_id
             }
     )
+    return(prtasks)
+
+
+def set_id_prefix_to_task_id(tasks, prefix):
     for t in tasks:
-        t['Outline number'] = proj_id + "." + str(t['Outline number'])
-for t in tasks:
-    if t['status'] != 'deleted':  # Ignore deleted tasks
-        due = t['due']
-        start = t['scheduled']
-        # Process dates
-        if due is None:
-            due = default_time_end
-        else:
-            due = due.date()
-        if start is None:
-            start = default_time_start
-        else:
-            start = start.date()
-        due_s = due.strftime('%d/%m/%y')
-        start_s = start.strftime('%d/%m/%y')
-        # Calculate num days
-        if not (due is None or start is None):
-            # days = (due - start).days
-            days = busday_count(start, due)
-        else:
-            days = 0
-        # Append tasks
-        prtasks.append(
-                {
-                    'ID': t['new_id'], 'Name': t['description'],
-                    'Begin date': start_s, 'End date': due_s,
-                    'Duration': days, "Outline number": t['Outline number']
-                }
-        )
-        # print(t['project'], t['id'], t['description'], t['depends'])
-try:
-    with open(csv_file, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-        writer.writeheader()
-        for data in prtasks:
-            writer.writerow(data)
-except IOError:
-    print("I/O error")
+        t['Outline number'] = prefix + "." + str(t['Outline number'])
+    return(tasks)
+
+
+def tasks_to_row(prtasks, tasks, tw, default_time_end):
+    for t in tasks:
+        if t['status'] != 'deleted':  # Ignore deleted tasks
+            due = t['due']
+            start = t['scheduled']
+            # Process dates
+            if due is None:
+                if t.completed:
+                    due = get_task_end(tw, t)
+                else:
+                    due = default_time_end
+            else:
+                due = due.date()
+            if start is None:
+                # Not available -> set the creation date
+                start = t['entry'].date()  
+            else:
+                start = start.date()
+            due_s = due.strftime('%d/%m/%y')
+            start_s = start.strftime('%d/%m/%y')
+            # Calculate num days
+            if not (due is None or start is None):
+                # days = (due - start).days
+                days = busday_count(start, due)
+            else:
+                days = 0
+            # Append tasks
+            prtasks.append(
+                    {
+                        'ID': t['new_id'], 'Name': t['description'],
+                        'Begin date': start_s, 'End date': due_s,
+                        'Duration': days, "Outline number": t['Outline number']
+                    }
+            )
+            # print(t['project'], t['id'], t['description'], t['depends'])
+    return(prtasks)
+
+
+def rows_to_csv(prtasks, filename):
+    csv_columns = [
+            'ID', 'Name', 'Begin date', 'End date', 'Duration',
+            'Completion', 'Cost', 'Coordinator', 'Predecessors',
+            'Outline number', 'Resources', 'Assignments', 'Task color',
+            'Web Link', 'Notes'
+    ]
+    try:
+        with open(filename, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+            writer.writeheader()
+            for data in prtasks:
+                writer.writerow(data)
+    except IOError:
+        print("I/O error")
+
+
+###################################### MAIN ###################################
+def main():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument(
+            '--project', '-p', metavar="[Project name]", type=str,
+            help='Set project [Mandatory]'
+    )
+    parser.add_argument(
+            '--set_id', '-i', type=str, default="", metavar="ID",
+            help="Set project as task with this id. TODO: This should be bool"
+    )
+    parser.add_argument(
+            '--output', '-o', type=str, default="tasks_gantt.csv",
+            help="CSV file for output"
+    )
+
+    args = parser.parse_args()
+    print(args)
+    print(args.project)
+    if args.project is None:
+        print("Project is required")
+        parser.print_help()
+        exit()
+
+    # Input config
+    project = args.project
+    proj_id = args.set_id   # "" -> project is not displayed
+    csv_file = args.output
+
+    # Default start time
+    # madrid = pytz.timezone("Europe/Madrid")
+    default_time_start = datetime.date(2018, 1, 1)
+    default_time_end = datetime.date(datetime.date.today().year, 12, 31)
+
+    # Taskwarrior start
+    tw = TaskWarrior()
+    tasks = tw.tasks.filter("project:" + project)
+
+    id_list = dict()
+    if proj_id != "":
+        proj_id = '0'
+        id_list[project] = proj_id
+
+    tasks = build_numbering(tasks, id_list)
+    prtasks = []
+
+    if proj_id != "":
+        print("PROJECT ID SET")
+        prtasks = add_project(
+                prtasks, proj_id, project, default_time_start,
+                default_time_end)
+        tasks = set_id_prefix_to_task_id(tasks, proj_id)
+
+    prtasks = tasks_to_row(prtasks, tasks, tw, default_time_end)
+
+    rows_to_csv(prtasks, csv_file)
+
+
+# Main call
+main()
