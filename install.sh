16a47b21620a7879276346317e58943338f8cb98
Alberto Gutiérrez Torre <alberto.gutierrez@bsc.es>
2023-01-09 21:09:58 +0100

    Added dunst

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo pacman -S gvim tlp i3-wm dmenu doublecmd-gtk2 tpacpi-bat powertop nmon \
     isync vdirsyncer khal firefox rxvt-unicode urxvt-perls i3blocks ncmpcpp \
     task timew stow base-devel xclip odt2txt sxiv bat vlc mpv mpc mpstat \
     newsboat python-keyring ipython python-tasklib zathura-pdf-mupdf \
-    zathura-djvu w3m python-future tmux mopidy python-pip
+    zathura-djvu w3m python-future tmux mopidy python-pip dunst
 
 
 # Manjaro
@@ -23,8 +23,9 @@ PIP="sudo pip install"
 #$PACAUR rstudio-desktop-bin
 
 # Install AUR stuff
-$PACAUR st-luke-git lf nerd-fonts-liberation-mono redshift-gtk-git \
+$PACAUR  lf nerd-fonts-liberation-mono redshift-gtk-git \
     syncthing-gtk i3blocks-contrib 
+# st-luke-git
 
 # Install pip stuff
 $PIP mopidy-dleyna mopidy-mpd mopidy-local mopidy-bandcamp
@@ -58,4 +59,11 @@ bash -c "vim -c 'PlugInstall'"
 ## Reload Xresources
 xrdb ~/.config/X11/Xresources
 
+# Install st
+mkdir -p ~/workspace/
 
+cd workspace
+git clone https://github.com/LukeSmithxyz/st
+cd st
+sudo make install
+cd ../../
